837b97336ab7d14665d3b57e0ef8bd2d6c757a23
Bayram <91245046+Bayram13@users.noreply.github.com>
2025-07-13 21:09:20 +0400

    Delete lib/defillama.ts

--- lib/defillama.ts
+++
@@ -1,5 +0,0 @@
-export async function fetchBridgeInfo(address: string) {
-  const res = await fetch(`https://coins.llama.fi/prices/current/ethereum:${address}`)
-  const data = await res.json()
-  return data.coins ? Object.keys(data.coins) : []
-}
